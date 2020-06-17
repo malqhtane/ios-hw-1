d52c04ee04df086908cebb73a71427d120bcfda3
mariam <d942a494867f6ca76d44e353bac882b1c988f1d6>
2020-06-17 16:44:33 +0300

    mariam cw
    done

---
+++ untitled folder/cw1-mariam582 9.28.08 PM.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+var str = "Hello, playground"
+var name = " Mariam "
+var hobby1 = "Reading"
+var hobby2 = "Swimming"
+var age = 17
+var height = 1.65
+print (name,hobby1,hobby2,age,height)
+//
+var friendHobby1 = "Reading"
+var friendHobby2 = "cooking"
+var freindage = 18
+let freindheight = 1.65
+var freindname = "Maha"
+//
+var age_difference = age - freindage
+var hieght_fifference = height == freindheight
+print("my name is :",name ," , and my age is \(age), and my height is:\(height) , my hobbies are : \(hobby1), \(hobby2)")
+ name == freindname
+name != freindname
+
+hobby1 == friendHobby1
+hobby1 != friendHobby1
+hobby2 == friendHobby2
+hobby2 != friendHobby2
+hobby1 == friendHobby2
+hobby1 != friendHobby2
+hobby2 == friendHobby1
+hobby2 != friendHobby1
+
+age > freindage
+age < freindage
+age >= freindage
+age <= freindage
+age == freindage
+age != freindage
+
+let bestfrirends =
+hobby1 == friendHobby1 ||
+hobby1 == friendHobby2 ||
+hobby2 == friendHobby1 ||
+hobby2 == friendHobby2 ||
+name == freindname
